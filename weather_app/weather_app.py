ac790ab2e5199d794d20d26ff7f1077c3bf07218
nathan.berger <berger.nathand@gmail.com>
2022-02-27 19:05:52 -0600

    Saving changes to weather app

--- weather_app/weather_app.py
+++ weather_app/weather_app.py
@@ -1,6 +1,33 @@
 
-api.openweathermap.org/data/2.5/weather?q={chicago}&appid={055d35a864a20033813a9359739a8f5a}
+from configparser import ConfigParser
+import argparse
+
+def _get_api_key():
+    config = ConfigParser()
+    config.read("secrets.ini")
+    return config["openweather"]["api_key"]
+
+def read_user_cli_args():
+    """Handles the CLI user interactions.
+
+    Returns:
+        argparse.Namespace: Populated namespace object
+    """
+    parser = argparse.ArgumentParser(
+        description="gets weather and temperature information for a city"
+    )
+    parser.add_argument(
+        "city", nargs="+", type=str, help="enter the city name"
+    )
+    parser.add_argument(
+        "-i",
+        "--imperial",
+        action="store_true",
+        help="display the temperature in imperial units",
+    )
+    return parser.parse_args()
 
 
-
-api.openweathermap.org/data/2.5/weather?lat={45}&lon={45}&appid={e92158e8fe467797704f908d62fccd85}
+if __name__ == "__main__":
+    user_args = read_user_cli_args()
+    print(user_args.city, user_args.imperial)
