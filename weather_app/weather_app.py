0c69b193cf5b514c9f0a9ab5581b38abad1b818c
nathan.berger <berger.nathand@gmail.com>
2022-02-27 18:39:56 -0600

    Add project files.

---
+++ weather_app/weather_app.py
@@ -0,0 +1,6 @@
+
+api.openweathermap.org/data/2.5/weather?q={chicago}&appid={055d35a864a20033813a9359739a8f5a}
+
+
+
+api.openweathermap.org/data/2.5/weather?lat={45}&lon={45}&appid={e92158e8fe467797704f908d62fccd85}
